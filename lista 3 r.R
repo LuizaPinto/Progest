ecc426d8dfb9bea3796f41872f9979d8f5da7861
LuizaPinto <54412214+LuizaPinto@users.noreply.github.com>
2019-08-28 15:30:33 -0300

    Update lista 3 r.R

--- lista 3 r.R
+++ lista 3 r.R
@@ -1,4 +1,36 @@
 
+##Exercicio extra
+
+fatoracao <- function(k){
+  n = k
+  fatores <- NULL
+  f=2 
+  while(n!= 1){
+    while(n%%f==0){
+      fatores= c(fatores,f)
+      n=n/f
+    }
+    f = f + 1
+  }
+  return(fatores)
+}
+fatoracao(25)
+fatoracao(24)
+##2a
+
+e_primo <- function(k){
+  if(k==1||(k==2))
+    return(T)
+  for(n in 2:(k-1)){
+    if(k%%n==0){
+      
+    }
+  }
+  return(F)
+}
+e_primo()
+###1
+
 menor <- function(a,b){
   if(a<b)
     return (a)
@@ -30,8 +62,9 @@ triangulo <- function(a,b,c){
       return("triangulo isoscles")
   }
 }
-#3
+#4
 
+##a
 multiplos_de_m <- function(m,n){
   vet <-  NULL
   for(i in 1:n){
@@ -39,7 +72,7 @@ multiplos_de_m <- function(m,n){
   }
   return(vet)
 }
-#5
+##b
 multiplos_de_m2 <- function(m,k){
   vet<- NULL
   i=1
@@ -53,10 +86,57 @@ multiplos_de_m2 <- function(m,k){
   return(vet)
 }
 
+##c
+
+multiplos_de_mqk <- function(m,k){
+  i=1
+  c=0
+  repeat{
+    if (m*i>=k)
+      break
+    c=c+1
+    i=i+1
+  }
+  
+  ##5
+  matriz12 <- function(n){
+  if(n<=0 && n%%1 !=0)
+    stop("O numero tem que ser natural")
+  m=matrix(0,nrow=n,ncol=n,byrow=F)
+  for (i in 1:n) {
+    for(j in 1:n){
+      if(i%%2== 0){
+        m[i,j]=2
+      }else{
+        m[i,j]=1
+      }
+    }
+    
+  }
+return(m)
+}
+  return(c)
+}
+
+##b
+matriz123<-function(n){
+  if(n<=0 || n%%1!=0)
+    stop("Digite um número inteiro positivo")
+  m=matrix(0,nrow=n,ncol=n,byrow=F)
+  for(i in 1:n){
+    for(j in 1:n){
+        m[j,i]=i
+      
+      
+    }
+    
+  }
+  return(m)
+}
 
 
+matriz123(5)
 
 
-
-
+##
 
