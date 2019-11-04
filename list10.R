97637801e637fe33f0bd57c48299215ff4899167
LuizaPinto <54412214+LuizaPinto@users.noreply.github.com>
2019-11-04 15:37:31 -0300

    Update list10.R

--- list10.R
+++ list10.R
@@ -19,3 +19,31 @@ curve(pol_exp(x,n=2),add=T,col="violet")
 curve(pol_exp(x,n=3),add=T,col="red")
 curve(pol_exp(x,n=4),add=T,col="blue")
 curve(pol_exp(x,n=5),add=T,col="green")
+
+
+
+
+
+#2
+
+aproex<- function(x,incremento){
+  fat <- 1
+  aprox<- 1
+  for (i in 1:10){
+    fat <- fat*i
+    aprox <-  aprox + (x^i)/fat
+    
+  }
+  repeat{
+    i <- i+1
+    fat <- fat*i
+    parcela <- x^i/fat
+    if(abs(parcela)< incremento){
+      return(list(aprox+parcela,i))
+      
+    }
+    aprox <-  aprox + parcela
+  }
+}
+
+aproex(1, 0.000000000001)  
